7eb152b798663028bdcfd260365e4e202d752ed5
Usmon Kurbanbaev <usmonkurbanbaev1@gmail.com>
2023-01-05 21:20:23 +0500

    task5

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -1,6 +1,12 @@
 package lang.print.gaps.task5;
 
 public class NumberDivider {
-    void divide(int toBeDivided) {
+    public static void divide(int toBeDivided) {
+        double divider = toBeDivided / 5.0;
+        System.out.println(divider);
+    }
+
+    public static void main(String[] args) {
+        divide(19);
     }
 }
--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -1,6 +1,11 @@
 package lang.print.gaps.task5;
 
 public class TimeConvertor {
-    public void convert(float minutes) {
+    public static void convert(float minutes) {
+        float seconds = 60 * minutes;
+        System.out.println(seconds);
+    }
+    public static void main(String[] args) {
+        convert(11.5F);
     }
 }
